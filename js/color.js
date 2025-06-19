f2bc48747cb2defd6fe1ffc43d63a58751a76535
Caius Iulius Caesar <caiusiuliuscaesar84@gmail.com>
2025-06-19 11:55:24 +0200

    ottimizzazione colori

--- js/color.js
+++ js/color.js
@@ -9,21 +9,19 @@ export class Color {
         this.b = b;
     }
 
-    add(col) {
-        return new Color(this.r + col.r, this.g + col.g, this.b + col.b);
-    }
-
-    mul(factor) {
-        return new Color(factor*this.r, factor*this.g, factor*this.b)
-    }
-
     static weighed_sum(weights_cols) {
-        const weights_sum = weights_cols.reduce((acc, wc) => [
-            acc[0] + wc[0],
-            acc[1].add(wc[1].mul(wc[0]))
-        ], [0, new Color(0,0,0)]);
+        let r = 0;
+        let g = 0;
+        let b = 0;
+        let weights_sum = 0;
+        for (const wc of weights_cols) {
+            r += wc[0] * wc[1].r;
+            g += wc[0] * wc[1].g;
+            b += wc[0] * wc[1].b;
+            weights_sum += wc[0];
+        }
 
-        return weights_sum[1].mul(1 / weights_sum[0]);
+        return new Color(r/weights_sum, g/weights_sum, b/weights_sum);
     }
 
     get style() {
@@ -35,33 +33,50 @@ export class Color {
 }
 
 export class WaveCanvas extends Wave {
+    static CONTRADICTION_COLOR = new Color(255, 0, 255);
+
     constructor(rows, cols, tileset, canvas, palette) {
         super(rows, cols, tileset);
         this.palette = palette;
         this.canvas = canvas;
     }
 
+    clear() {
+        const ctx = this.canvas.getContext("2d");
+        ctx.fillStyle = "white";
+        ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
+    }
+
     render() {
         const ctx = this.canvas.getContext("2d");
-        const tile_size = 10;
         const rows = this.tileset.wrap_rows ? this.rows : this.rows - 1;
         const cols = this.tileset.wrap_cols ? this.cols : this.cols - 1;
 
-        this.canvas.width = cols * tile_size;
-        this.canvas.height = rows * tile_size;
+        const tile_size = Math.min(
+            this.canvas.width / cols,
+            this.canvas.height / rows
+        );
+
+        const width_bias = (this.canvas.width - tile_size * cols) / 2;
+        const height_bias = (this.canvas.height - tile_size * rows) / 2;
 
         for (let i = 0; i < rows; i++) {
             for (let j = 0; j < cols; j++) {
                 const tile = this.grid[i][j];
-                if (tile.length === 0) continue;
 
-                const wc = tile.map((t) => {
-                    return [this.tileset.get_weight(t), this.palette[this.tileset.tiles[t].center]];
-                });
-                const color = Color.weighed_sum(wc);
+                if (tile.length === 0) {
+                    ctx.fillStyle = WaveCanvas.CONTRADICTION_COLOR.style;
+                } else {
+                    const wc = tile.map((t) => {
+                        return [this.tileset.get_weight(t), this.palette[this.tileset.tiles[t].center]];
+                    });
 
-                ctx.fillStyle = color.style;
-                ctx.fillRect(j * tile_size, i * tile_size, tile_size, tile_size);
+                    const color = Color.weighed_sum(wc);
+
+                    ctx.fillStyle = color.style;
+                }
+
+                ctx.fillRect(width_bias + j * tile_size, height_bias + i * tile_size, tile_size, tile_size);
             }
         }
     }
